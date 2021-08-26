06b37c7883fab99d31d6dc125b0b8b53e53bd3d6
dB SPL <deltabravo15ga@gmail.com>
2021-08-26 15:57:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dB-SPL/pyAMP",
-	keywords=['flamp', fldigi', 'Python', 'Opensource'],
+	keywords=['flamp', 'fldigi', 'Python', 'Opensource'],
     packages=setuptools.find_packages(),
 	install_requires=[
 		'crcmod',
